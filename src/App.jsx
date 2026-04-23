e3763443016678292846ff5d24daad45897bf1de
Sam Khan <samadkhan040204@gmail.com>
2026-04-23 15:04:48 +0530

    file structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="absolute inset-0 w-auto h-auto bg-blue-500 flex items-center justify-center">
+      <h1>App</h1>
+    </div>
+  )
+}
+
+export default App
