85fc34154190dd25ba08c31e1c045c55cfed20bb
komal shrawankar <shrawankarkomal9@gmail.com>
2026-04-25 12:56:26 +0530

    fotter-updates

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -1,53 +1,59 @@
-/* eslint-disable no-unused-vars */
 import React from "react";
-import { Phone, Mail, MapPin } from "lucide-react";
+import logo from "../assets/common-images/abco-logo.png";
+
+import phoneIcon from "../assets/common-images/phone.png";
+import mailIcon from "../assets/common-images/mail.png";
+import mapIcon from "../assets/common-images/map.png";
+
 
 const Footer = () => {
   return (
-    <footer className="bg-gradient-to-r from-[#081b33] to-[#0d2a4d] text-white px-12 py-14">
-      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-12">
+    <footer className="bg-gradient-to-r from-[#071a2f] to-[#0c2746] text-white px-10 py-10">
 
-        {/* LEFT SECTION */}
+      <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-4 gap-10 justify-items-center">
+
+        {/* LEFT */}
         <div>
-          {/* Logo */}
-          <div className="mb-4">
-            <img
-              src="/src/assets/common-images/abco-logo.png"
-              alt="ABCO Computers"
-              className="h-12"
-            />
-          </div>
+          {/* LOGO */}
+          <img
+            src={logo}
+            alt="ABCO Computers"
+            className="h-12 mb-4"
+          />
 
-          {/* Description */}
-          <p className="text-sm text-gray-300 leading-relaxed mb-6">
+          {/* TEXT */}
+          <p className="text-[13px] text-white leading-relaxed mb-5 max-w-xs">
             ABCO Computers supports aspiring IT professionals with skill-based
             training, guidance, and career opportunities to succeed in the
             global tech industry.
           </p>
 
-          {/* Divider */}
-          <div className="w-full h-[1px] bg-gray-400 mb-6"></div>
+          {/* DIVIDER */}
+          <div className="w-full h-[1px] bg-white mb-5"></div>
 
-          {/* Contact */}
-          <div className="space-y-4 text-sm text-gray-300">
+          {/* CONTACT */}
+          <div className="space-y-4 text-[13px] text-white ">
 
             <div className="flex items-center gap-3">
-              <div className="bg-white text-black p-2 rounded-full">
-                <Phone size={14} />
+              <div className=" flex items-center justify-center 
+   ">
+                <img src={phoneIcon} alt="phone" className="w-12 h-10 object-contain" />
               </div>
               <span>+91-9370181988</span>
             </div>
 
             <div className="flex items-center gap-3">
-              <div className="bg-white text-black p-2 rounded-full">
-                <Mail size={14} />
+              <div className=" flex items-center justify-center 
+  rounded-full ">
+                <img src={mailIcon} alt="mail" className="w-12 h-10 object-contain" />
               </div>
               <span>hr-manager@abcocomputers.com</span>
             </div>
 
             <div className="flex items-start gap-3">
-              <div className="bg-white text-black p-2 rounded-full mt-1">
-                <MapPin size={14} />
+              <div className="flex items-center justify-center 
+  rounded-full bg-white mt-1">
+                <img src={mapIcon} alt="location" className="w-12 h-10 object-contain" />
               </div>
               <span className="leading-relaxed">
                 Plot No.47, 3rd Floor, Katol Road, Khamla <br />
@@ -59,39 +65,39 @@ const Footer = () => {
         </div>
 
         {/* MENU */}
-        <div className="space-y-6 justify-items-start flex flex-col items-start">
-          <h3 className="text-xxl font-semibold mb-6">Menu</h3>
-          <ul className="space-y-3 text-gray-300 text-sm flex flex-col items-start">
-            <li className="hover:text-white cursor-pointer text-lg">Home</li>
-            <li className="hover:text-white cursor-pointer text-lg">About us</li>
-            <li className="hover:text-white cursor-pointer text-lg">Services</li>
-            <li className="hover:text-white cursor-pointer text-lg">Department</li>
-            <li className="hover:text-white cursor-pointer text-lg">Learning</li>
-            <li className="hover:text-white cursor-pointer text-lg">Contact us</li>
+      <div >
+          <h3 className="text-[22px] font-semibold mb-5">Menu</h3>
+          <ul className="space-y-3 text-[15px] text-white ">
+            <li className="hover:text-white cursor-pointer">Home</li>
+            <li className="hover:text-white cursor-pointer">About us</li>
+            <li className="hover:text-white cursor-pointer">Services</li>
+            <li className="hover:text-white cursor-pointer">Department</li>
+            <li className="hover:text-white cursor-pointer">Learning</li>
+            <li className="hover:text-white cursor-pointer">Contact us</li>
           </ul>
         </div>
 
         {/* EXPLORE */}
-        <div className="space-y-6 justify-items-start flex flex-col items-start">
-          <h3 className="text-xxl font-semibold mb-6">Explore</h3>
-          <ul className="space-y-3 text-gray-300 text-sm flex flex-col items-start">
-            <li className="hover:text-white cursor-pointer text-lg">Careers</li>
-            <li className="hover:text-white cursor-pointer text-lg">Blogs</li>
-            <li className="hover:text-white cursor-pointer text-lg">Success Stories</li>
+    <div>
+          <h3 className="text-[22px] font-semibold mb-5">Explore</h3>
+          <ul className="space-y-3 text-[15px] text-white">
+            <li className="hover:text-white cursor-pointer">Careers</li>
+            <li className="hover:text-white cursor-pointer">Blogs</li>
+            <li className="hover:text-white cursor-pointer">Success Stories</li>
           </ul>
         </div>
 
         {/* POLICIES */}
-        <div>
-          <h3 className="text-xxl font-semibold mb-6">Policies</h3>
-          <ul className="space-y-3 text-gray-300 text-sm flex flex-col items-start">
-            <li className="hover:text-white cursor-pointer text-lg">
+       <div>
+          <h3 className="text-[22px] font-semibold mb-5">Policies</h3>
+          <ul className="space-y-3 text-[15px] text-white ">
+            <li className="hover:text-white cursor-pointer">
               Term & Condition
             </li>
-            <li className="hover:text-white cursor-pointer text-lg">
+            <li className="hover:text-white cursor-pointer">
               Privacy Policy
             </li>
-            <li className="hover:text-white cursor-pointer text-lg">
+            <li className="hover:text-white cursor-pointer">
               Refund & Return Policy
             </li>
           </ul>
