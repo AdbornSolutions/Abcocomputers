3c6567c754c6fdea286d90fed6cb0e35baf3d0b6
Sam Khan <samadkhan040204@gmail.com>
2026-04-25 13:58:12 +0530

    routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-/* eslint-disable no-unused-vars */
-import React from 'react'
-import Home from './Page/Home'
-import Navbar from './Component/Navbar'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./Component/Navbar";
+
+// Pages
+import Home from "./Page/Home";
+import About from "./Page/About";
+import Contactus from "./Page/Contactus";
+import Services from "./Page/Services/Services";
+import Department from "./Page/Departments/Department";
+import Learning from "./Page/Learning";
 
 const App = () => {
   return (
     <>
-    <Home/>
-    
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contactus" element={<Contactus />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/department" element={<Department />} />
+        <Route path="/learning" element={<Learning />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
